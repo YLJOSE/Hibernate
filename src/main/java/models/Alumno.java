2b7091802ec96d18a8191333f5dbfac9f31d8401
YLJOSE <joserojasr47@gmail.com>
2025-02-11 16:44:32 +0100

    nuevo

--- src/main/java/models/Alumno.java
+++ src/main/java/models/Alumno.java
@@ -14,4 +14,35 @@ public class Alumno {
 
     public Alumno(){}
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getAverageMark() {
+        return averageMark;
+    }
+
+    public void setAverageMark(float averageMark) {
+        this.averageMark = averageMark;
+    }
 }
